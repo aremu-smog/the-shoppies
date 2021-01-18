70709425d7dec0595f4e6baa20e1c1ed552097b6
Aremu Smog <aremuoluwagbamila@gmail.com>
2021-01-18 10:55:51 +0100

    Remove purge

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Error404 = () => (
+  
+        <h1>404: Page not found</h1>
+
+)
+
+export default Error404
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/**/*.{js}'],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
